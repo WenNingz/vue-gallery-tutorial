25826ae7abda07a2088d0f0a83b2b6fbc4b806e8
Ning <wenning.me@gmail.com>
2018-07-16 20:16:02 +0900

    Finish tutorial

---
+++ src/components/ImageList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <a @click="emitImage(image)" v-for="image in images" v-bind:key="image.id" href="#">
+      <div class="col-md-3 col-sm-4 col-xs-6">
+        <img class="img-responsive" :src="image.thumbnail" />
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+import SingleImage from './SingleImage'
+export default {
+  props: ['images'],
+  components: { SingleImage },
+  methods: {
+    emitImage: function (image) {
+      this.$emit('emitImage', image)
+    }
+  }
+}
+</script>
